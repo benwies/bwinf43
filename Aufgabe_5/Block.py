d3d54288116060ed77ed545463b34a065248b458
RadKripe <leonard.beinlich@gmail.com>
2024-09-09 18:19:09 +0200

    Is der Grundstein habe bisher noch KP ob der überhaupt sinnvoll ist

---
+++ Aufgabe_5/Block.py
@@ -0,0 +1,18 @@
+# Erstellt die einzelnen Blöcke
+
+class Block():
+    def __init__(self, interval):
+        self.interval = int(interval)
+        self.time = int(0)
+        self.closed = True
+
+    
+# Überprüft ob die Tür gerade geschlossen oder offen ist
+    def checkStatues(self):
+        if self.time % self.interval == 0:
+            if self.time/self.interval%2 == 0:
+                self.closed == True
+                print("closed")
+            else:
+                self.closed == False
+                print("open")
--- Aufgabe_5/dasAegiptischeGrabmal.py
+++ Aufgabe_5/dasAegiptischeGrabmal.py
@@ -1,9 +1,26 @@
-listOfBlocks = []
+import keyboard
+import Block as bl
 
+listOfBlocks = []
+# hier werden die einzelnen Intzervalle für die Blöcke eingegeben und ein Objekt Block wird erstellt mit dem jeweiligen Intervall als Variable
 userInput = -1
-print("Bitte geben sie  Steinblock zeiten ein falls fertig geben sie fertig ein ")
+print("Bitte geben sie  Steinblock Intervalle ein falls fertig geben sie fertig ein ")
 while (userInput != "fertig"):
     userInput = input()
     if userInput.isdigit() == True:
-        listOfBlocks.append(userInput)
-print(listOfBlocks)
+        block = bl.Block(userInput)
+        listOfBlocks.append(block)
+
+# Setzt die Zeit immer um 1 Hoch für jedes Objekt einzeln
+def time():
+    for i in range(len(listOfBlocks)):
+        listOfBlocks[i].time = listOfBlocks[i].time +1
+        print(listOfBlocks[i].time)
+        listOfBlocks[i].checkStatues()
+    pass
+
+while keyboard.read_key() != "e":
+    if keyboard.read_key() == "q":
+        time()
+        
+
