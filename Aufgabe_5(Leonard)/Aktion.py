65890c6998ad137c6698c7815783d9a60101cb25
RadKripe <leonard.beinlich@gmail.com>
2024-11-17 12:11:55 +0100

    t

--- Aufgabe_5(Leonard)/Aktion.py
+++ Aufgabe_5(Leonard)/Aktion.py
@@ -6,7 +6,7 @@ from ListeAktionen import ListeAktionen
 class Aktion(ListeAktionen, ListBlocks):
     def __init__(self,zuWarten,position):
         #wartenist mind 1 da Türen Intervall < 0 ist und sonst die Rechnung nicht Fuunktioniert da eine gweisse Grundzeit gegeben werden muss
-        self.warten = int(1)
+        self.warten = int(0)
         self.bewegt = int(0)
         self.zuWarten = int(zuWarten)
         self.position = int(position)
@@ -15,10 +15,9 @@ class Aktion(ListeAktionen, ListBlocks):
     def move(self,time,liste):
         end = self.zuWarten+self.position
         while ListBlocks.checkStatusFromTo(self.position,end,time) != True :
-            self.warten =+ int(1)
+            self.warten = self.warten + int(1)
             time = time + 1
         self.bewegt = self.zuWarten    
-        self.warten =+ 1
-        time =+ 1
+        # time = time + 1
 
         
--- Aufgabe_5(Leonard)/dasAegiptischeGrabmal.py
+++ Aufgabe_5(Leonard)/dasAegiptischeGrabmal.py
@@ -5,11 +5,14 @@ import Aktion as ak
 
 liste = la.ListeAktionen()
 blöcke = lb.ListBlocks()
-blöcke.addBlock(20)
 blöcke.addBlock(3)
 blöcke.addBlock(3)
-blöcke.addBlock(4)
-blöcke.printList()
+blöcke.addBlock(3)
+blöcke.addBlock(3)
+blöcke.addBlock(3)
+blöcke.addBlock(3)
+blöcke.addBlock(3)
+blöcke.addBlock(3)
 
 # while liste.getPosition()-1 <= len(blöcke.listOfBlocks):
 print("Huan")
@@ -17,6 +20,11 @@ while(liste.getPosition() != len(blöcke.listOfBlocks)):
     time = liste.getTime()
     liste.list[-1].move(time,blöcke.listOfBlocks)
     liste.extendList()
-    print(len(liste.list))
+    print("Länge Blöcke: ", len(blöcke.listOfBlocks)," Länge Aktionen: ", liste.getPosition())
+    print("time" , liste.getTime())
     print ("Position:" ,liste.getPosition())
 
+for i in range(len(liste.list)):
+    print(liste.list[i].warten)
+    
+print("Länge Liste: ",len(liste.list))
